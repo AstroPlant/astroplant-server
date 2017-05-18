8a1396aedb556447a1f55352a9a055e92449bcf5
Beskhue <thomas@kepow.org>
2017-05-18 19:19:41 +0200

    Set up apps.

--- server/settings.py
+++ server/settings.py
@@ -29,12 +29,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third part apps
+    'bootstrap3',
+    # Own apps
+    'website',
 ]
 
 MIDDLEWARE = [
