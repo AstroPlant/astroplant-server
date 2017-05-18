16b94666d8794daa0366b5ebf85646847b2cf0a1
Beskhue <thomas@kepow.org>
2017-05-18 22:11:21 +0200

    Improve templates and simplify HTML. Add index view.

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Website index.")
+    context = {}
+    return render(request, 'website/index.html', context)
     
