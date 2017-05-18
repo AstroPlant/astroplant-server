0a74fefc25ac9adf33f0d21b258e5da6ecd5e723
Beskhue <thomas@kepow.org>
2017-05-19 00:17:45 +0200

    Add start of user registration.

--- server/settings.py
+++ server/settings.py
@@ -124,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Accounts and registration
+ACCOUNT_ACTIVATION_DAYS = 14
--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^map$', views.map, name='map'),
+    url(r'^accounts/', include('registration.backends.hmac.urls', namespace='accounts')),
 ]
