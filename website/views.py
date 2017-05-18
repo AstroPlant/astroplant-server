d09db17156550a867f0b7153438b5ac2e7dd1d75
Beskhue <thomas@kepow.org>
2017-05-18 23:38:53 +0200

    Add start of map page.

--- website/views.py
+++ website/views.py
@@ -4,3 +4,10 @@ def index(request):
     context = {}
     return render(request, 'website/index.html', context)
     
+def map(request):
+    # Todo: programmatically add the markers of the AstroPlant kits.
+    # Note that we don't use GeoDjango; it requires a heavy gdal setup. All
+    # we need is a simple map, and a full gdal setup would just make deployment
+    # more difficult
+    context = {}
+    return render(request,'website/map.html', context)
