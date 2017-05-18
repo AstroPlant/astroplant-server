5b681bf896307328af169960bd03b92c2a30e80c
Beskhue <thomas@kepow.org>
2017-05-18 20:19:25 +0200

    Add SCSS.

---
+++ website/static/website/config.rb
@@ -0,0 +1,12 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "_sass"
+
+# :nested > :expanded, but :expanded is easier to read
+output_style = :expanded
+
+line_comments = false
+asset_cache_buster = :none
+cache = false
