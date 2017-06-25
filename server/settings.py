1afa7622208465514270d391af8d885c0b09063b
Beskhue <thomas@kepow.org>
2017-06-25 19:58:43 +0200

    Allow localhost.

--- server/settings.py
+++ server/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = server.secretsettings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["astroplant.kepow.org", "astroplant.io"]
+ALLOWED_HOSTS = ["localhost", "astroplant.kepow.org", "astroplant.io"]
 
 
 # Application definition
