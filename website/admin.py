4e0ac3948c87d958409d974e7196f8b36b5bb198
Beskhue <thomas@kepow.org>
2017-09-04 20:21:53 +0200

    Add kit memberships to admin site.

--- website/admin.py
+++ website/admin.py
@@ -7,6 +7,11 @@ class KitAdmin(admin.ModelAdmin):
     list_display = ('serial', 'type', 'name')
     ordering = ('serial', 'name', 'id')
 
+@admin.register(models.KitMembership)
+class KitMembership(admin.ModelAdmin):
+    list_display = ('kit', 'user', 'date_time_linked')
+    ordering = ('kit', 'user')
+
 admin.site.register(models.SensorType)
 admin.site.register(models.Experiment)
 admin.site.register(models.Sensor)
