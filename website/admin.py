ffcc5dda76ee1c46dc7e6da3b23f6bf2bee9141c
Beskhue <thomas@kepow.org>
2017-09-04 19:57:17 +0200

    Register models in admin view.

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from backend import models
+
+admin.site.register(models.Kit)
+admin.site.register(models.KitProfile)
+admin.site.register(models.SensorType)
+admin.site.register(models.Experiment)
+admin.site.register(models.Sensor)
+admin.site.register(models.Measurement)
