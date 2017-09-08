f35176e742ccfba0c1a776b7cbdf6cfe883444d3
Beskhue <thomas@kepow.org>
2017-09-08 17:49:09 +0200

    Add Kit markers to the world map.

--- website/views.py
+++ website/views.py
@@ -6,17 +6,18 @@ from django.contrib.auth import views as auth_views
 from braces.views import AnonymousRequiredMixin, LoginRequiredMixin
 from registration.backends.hmac import views as registration_views
 
+import backend.models
+
 def index(request):
     context = {}
     return render(request, 'website/index.html', context)
     
 def map(request):
-    # Todo: programmatically add the markers of the AstroPlant kits.
     # Note that we don't use GeoDjango; it requires a heavy gdal setup. All
     # we need is a simple map, and a full gdal setup would just make deployment
     # more difficult
-    context = {}
-    messages.add_message(request, messages.ERROR, 'The map has not been implemented yet.')
+    context = {'kits': backend.models.Kit.objects.all()}
+
     return render(request,'website/map.html', context)
        
 @decorators.login_required
