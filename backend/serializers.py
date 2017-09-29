4112928a276e21b0eec04cd734c5d9dc6423143e
Beskhue <thomas@kepow.org>
2017-09-29 23:11:20 +0200

    Rename hyperlinked serializers such that their name reflects they are hyperlinked.

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,22 +1,27 @@
 from backend import models
 from rest_framework import serializers
 
-class KitSerializer(serializers.HyperlinkedModelSerializer):
+class HyperlinkedKitSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Kit
         fields = ('url', 'type', 'name', 'description', 'latitude', 'longitude', 'experiment_set')
 
-class ExperimentSerializer(serializers.HyperlinkedModelSerializer):
+class HyperlinkedExperimentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Experiment
         fields = ('url', 'kit', 'date_time_start', 'date_time_end')
 
-class SensorTypeSerializer(serializers.HyperlinkedModelSerializer):
+class HyperlinkedSensorTypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.SensorType
         fields = ('url', 'name', 'brand', 'type', 'unit')
 
-class MeasurementSerializer(serializers.HyperlinkedModelSerializer):
+class HyperlinkedMeasurementSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Measurement
+        fields = ('url', 'id', 'sensor_type', 'date_time', 'value')
+
+class MeasurementSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Measurement
         fields = ('url', 'id', 'sensor_type', 'date_time', 'value')
