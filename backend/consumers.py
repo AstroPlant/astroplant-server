993b8cd832472a920b771ff9bf0af4b4aac98e32
Beskhue <thomas@kepow.org>
2017-10-01 13:43:13 +0200

    Remove nested ifs.

--- backend/consumers.py
+++ backend/consumers.py
@@ -46,32 +46,37 @@ class MeasurementConsumer(channels.generic.websockets.JsonWebsocketConsumer):
         super().connect(message, **kwargs)
 
     def receive(self, content, multiplexer, **kwargs):
-        if "action" in content:
-            action = content['action']
-            if action == "subscribe":
-                # Subscribe user to kit measurement updates
-                if "kit" in content:
-                    try:
-                        kit = backend.models.Kit.objects.get(username=content['kit'], users=self.message.user)
-                        channels.Group("kit-measurements-%s" % kit.username).add(multiplexer.reply_channel)
-                        multiplexer.send({"action": "subscribe", "kit": kit.username})
-                    except:
-                        multiplexer.send({"error": "Kit not found or you do not have access to it."})
-                else:
-                    multiplexer.send({"error": "Kit to subscribe to not given."})
-            elif action == "publish":
-                # Publish a measurement
-                if "kit" in self.message.channel_session:
-                    try:
-                        measurement_serializer = backend.serializers.MeasurementSerializer(data=content['measurement'])
-                        measurement_serializer.is_valid(raise_exception = True)
-                        multiplexer.send({"success": "published"})
-                        self.group_send("kit-measurements-%s" % self.message.channel_session['kit'], measurement_serializer.data)
-                    except:
-                        multiplexer.send({"error": "You must provide a valid measurement.'."})
-                else:
-                    multiplexer.send({"error": "You must be a kit to publish measurements.'."})
-            else:
-                multiplexer.send({"error": "Unknown action: '%s'." % action})
-        else:
+        if "action" not in content:
             multiplexer.send({"error": "No action given."})
+            return
+        
+        action = content['action']
+        if action not in ("subscribe", "publish"):
+            multiplexer.send({"error": "Unknown action: '%s'." % action})
+            return
+
+        if action == "subscribe":
+            # Subscribe user to kit measurement updates
+            if "kit" not in content:
+                multiplexer.send({"error": "Kit to subscribe to not given."})
+                return
+
+            try:
+                kit = backend.models.Kit.objects.get(username=content['kit'], users=self.message.user)
+                channels.Group("kit-measurements-%s" % kit.username).add(multiplexer.reply_channel)
+                multiplexer.send({"action": "subscribe", "kit": kit.username})
+            except:
+                multiplexer.send({"error": "Kit not found or you do not have access to it."})
+        elif action == "publish":
+            # Publish a measurement
+            if "kit" not in self.message.channel_session:
+                multiplexer.send({"error": "You must be a kit to publish measurements.'."})
+                return
+
+            try:
+                measurement_serializer = backend.serializers.MeasurementSerializer(data=content['measurement'])
+                measurement_serializer.is_valid(raise_exception = True)
+                multiplexer.send({"success": "published"})
+                self.group_send("kit-measurements-%s" % self.message.channel_session['kit'], measurement_serializer.data)
+            except:
+                multiplexer.send({"error": "You must provide a valid measurement.'."})
