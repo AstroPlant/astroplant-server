1b35516ca1f7019f15452deb8295084371dcd0da
Beskhue <thomas@kepow.org>
2017-10-03 01:14:15 +0200

    Fix routing.

--- website/urls.py
+++ website/urls.py
@@ -5,11 +5,24 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^map/$', views.map, name='map'),
     url(r'^dashboard/', include([
-        url(r'$', views.dashboard, name='dashboard'),
-        url(r'(?P<kit_id>[0-9]+)/', include([
-            url(r'$', views.kit, name='kit'),
-            url(r'configure/$', views.kit),
+        url(r'^$', views.dashboard, name='dashboard'),
+        url(r'^add/$', views.kit_add, name='kit_add'),
+        url(r'^(?P<kit_id>[0-9]+)/', include([
+            url(r'^$', views.kit, name='kit'),
+            url(r'^configure/$', views.kit, name='kit_configure'),
         ])),
-        url(r'add/$', views.kit_add, name='kit_add'),
     ])),
 ]
+
+"""
+
+url(r'^dashboard/$', views.dashboard, name='dashboard'),
+url(r'^dashboard/', include([
+    url(r'(?P<kit_id>[0-9]+)/', include([
+        url(r'$', views.kit, name='kit'),
+        url(r'configure/$', views.kit, name='kit_configure'),
+    ])),
+    url(r'add/$', views.kit_add, name='kit_add'),
+])),
+
+"""
