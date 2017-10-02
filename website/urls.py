fb96090b462bfbd44fe95e6addc116794d34dbbb
Beskhue <thomas@kepow.org>
2017-10-03 00:46:13 +0200

    Simplify routing and show dashboard as active when on the dashboard page (does not work yet when on the kit page).

--- website/urls.py
+++ website/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^map/$', views.map, name='map'),
-    url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^dashboard/', include([
+        url(r'$', views.dashboard, name='dashboard'),
         url(r'(?P<kit_id>[0-9]+)/', include([
             url(r'$', views.kit, name='kit'),
             url(r'configure/$', views.kit),
