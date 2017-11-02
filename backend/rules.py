57da2ac18c8a21c8c6e1109842a929448ff897d0
Beskhue <thomas@kepow.org>
2017-11-02 02:24:05 +0100

    Add rules defining whether a user can view, assign and edit sensor definitions.

--- backend/rules.py
+++ backend/rules.py
@@ -31,7 +31,30 @@ def is_kit_member(user, kit):
 
     return len(kit.users.filter(pk=user.pk)) == 1
 
+@rules.predicate
+def is_public_sensor_definition(user, sensor_definition):
+    """
+    Predicate defining whether a sensor definition is public or not.
+    """
+    if not isinstance(kit, backend.models.SensorDefinition):
+        return False
+
+    return sensor_definition.public
+
+@rules.predicate
+def is_sensor_definition_owner(user, sensor_definition):
+    """
+    Predicate defining whether a user is the owner of sensor definition.
+    """
+    if not user or not isinstance(kit, backend.models.SensorDefinition):
+        return False
+
+    return sensor_definition.owner == user
+
 # Permissions
 rules.add_perm('backend.view_kit_dashboard', is_public_kit | is_target | is_kit_member)
 rules.add_perm('backend.subscribe_to_kit_measurements_websocket', is_public_kit | is_target | is_kit_member)
 rules.add_perm('backend.configure_kit', is_kit_member | is_target)
+rules.add_perm('backend.view_sensor_definition', is_public_sensor_definition | is_sensor_definition_owner)
+rules.add_perm('backend.assign_sensor_definition', is_public_sensor_definition | is_sensor_definition_owner)
+rules.add_perm('backend.edit_sensor_definition', is_sensor_definition_owner)
