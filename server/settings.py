68c8c98e3f656e61dccba280cbb884659b98e41e
Beskhue <thomas@kepow.org>
2017-11-06 23:05:29 +0100

    Fix typo.

--- server/settings.py
+++ server/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Third part apps
+    # Third party apps
     'bootstrap3',
     'braces',
     'django_gravatar',
