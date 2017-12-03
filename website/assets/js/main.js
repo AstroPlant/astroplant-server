1ea32f8184dd7eb42765baf43d835c01e6d09b4b
Beskhue <thomas@kepow.org>
2017-12-03 23:22:26 +0100

    Use a reconnecting websocket.

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -2,3 +2,4 @@ require('jquery');
 Popper = require('popper.js');
 require('bootstrap');
 require('bootstrap-confirmation2/bootstrap-confirmation.js');
+window.ReconnectingWebSocket = require('reconnecting-websocket');
