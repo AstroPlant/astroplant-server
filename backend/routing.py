93f6802e6405c76bb0811340060174f908317b9f
Beskhue <thomas@kepow.org>
2018-05-16 05:17:15 +0200

    Update measurement subscription route.

--- backend/routing.py
+++ backend/routing.py
@@ -9,7 +9,7 @@ import backend.consumers
 application = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(backend.middleware.JWTAuthMiddleware(
         URLRouter([
-            url(r"^measurements/subscribe/(?P<kit_name>[a-z0-9]+)/$", backend.consumers.MeasurementSubscribeConsumer),
+            url(r"^measurements/subscribe/(?P<kit_name>[\.a-z0-9]+)/$", backend.consumers.MeasurementSubscribeConsumer),
             url(r"^kit/$", backend.consumers.KitConsumer),
         ])
     )),
